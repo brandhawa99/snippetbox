5fb6a46f07ced55f42075274d31ab72b43e94db2
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-05-08 17:31:51 -0700

    added check for request type

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet...."))
 }
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 

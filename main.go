b8c5fe4226698417b8fffd80f3372c711e749832
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-05-30 00:08:29 -0700

    asdf

--- main.go
+++ main.go
@@ -16,10 +16,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet...."))
 }
+
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", "POST")
+
+		http.Error(w, "Method Not Allowed", 405)
+		return
 	}
 	w.Write([]byte("Create a new snippet..."))
 }

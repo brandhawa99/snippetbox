31d80d4ffe8ef6754da0d3872865e67a31b5a34e
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-05-31 23:23:39 -0700

    added snippet route

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -27,11 +29,22 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create a new snippet..."))
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet", showSnippet)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)

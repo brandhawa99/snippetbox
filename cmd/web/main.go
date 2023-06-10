f790b73f130ed524e33628e6b0d9a85c2450da36
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-06-10 13:14:27 -0700

    created methods against the application struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,10 +18,16 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
